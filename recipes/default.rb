64c76e82e8a33591d188e535a00b6db24a183e68
Russell Osborne <russosborn@gmail.com>
2015-01-19 10:31:33 -0500

    define install action.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,9 @@
 include_recipe 'apt' if node['platform_family'] == 'debian'
 
 node['wkhtmltopdf']['dependency_packages'].each do |p|
-  package p
+  package p do
+    action :install
+  end
 end
 
 include_recipe 'wkhtmltopdf::binary'
